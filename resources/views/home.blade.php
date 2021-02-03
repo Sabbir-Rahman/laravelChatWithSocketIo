71cffb4e5222bf832f48f8d50f7cfcb6dc9f942e
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-04 01:11:30 +0600

    unversioned file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row chat-row">
+    <div class="col-md-3">
+        <div class="users">
+            <h5>Users</h5>
+
+            <ul class="list-group list-chat-item">
+                <li class="chat-user-list">
+                    <a href="">
+                        Manohar
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
