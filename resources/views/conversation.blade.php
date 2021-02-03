eefd9a035438c8d17c7bb2a826d943eb435b9e5f
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-04 03:59:54 +0600

    full time and date is retrieved when hovered

--- resources/views/conversation.blade.php
+++ resources/views/conversation.blade.php
@@ -26,13 +26,44 @@
                 </ul>
             </div>
         </div>
-        <div class="col-md-9">
-            <h1>
-                Message section
-            </h1>
-            <p class="lead">
-                Chat Body Here
-            </p>
+        <div class="col-md-9 chat-section">
+            <div class="chat_header">
+                <div class="chat-header">
+                    <div class="chat-image">
+
+                    </div>
+
+                    <div class="chat-name font-weight-bold">
+                        {{ $user->name }}
+                        <i class="fa fa-circle user-status-head" title="away"
+                            id="userStatusHead{{$friendInfo->id}}"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat-body" id="chatBody">
+                <div class="message-listing" id="messageWrapper">
+                    <div class="row message align-items-center mb-2">
+                        <div class="col-md-12 user-info">
+                            <div class="chat-image">
+                                {!! makeImageFromName('Md sabbir') !!}
+                            </div>
+
+                            <div class="chat-name font-weight-bold">
+                                Md Sabbir
+                                <span class="small time text-gray-500 " title="2020-05-06 10:30 PM">
+                                    10:30 PM
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col-md-12 message-content">
+                            <div class="message-text">
+                                Message Here
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
